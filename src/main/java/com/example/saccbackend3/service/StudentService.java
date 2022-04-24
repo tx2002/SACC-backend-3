f4b9b650cb0af54d8bca449e5b134f39a3a9b1b4
Estrellayue <1959447917@qq.com>
2022-04-24 21:48:29 +0800

    dev-estrellayue

--- src/main/java/com/example/saccbackend3/service/StudentService.java
+++ src/main/java/com/example/saccbackend3/service/StudentService.java
@@ -1,7 +1,13 @@
 package com.example.saccbackend3.service;
 
 
+import com.example.saccbackend3.entity.Student;
+import com.example.saccbackend3.mapper.StudentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  *
  * @author TX
@@ -9,6 +15,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public interface StudentService {
-   
+
+    //新增一个学生
+    Boolean addStudent(Student student);
+
+    //根据id删除一个学生
+    Boolean deleteStudentById(String id);
+
+    //更新学生
+    Boolean updateStudent(Student student);
+
+    //查找所有学生
+    List<Student> listStudent();
+
+    //根据年龄段查找学生
+    List<Student> listStudentByAges(Integer age1, Integer age2);
 
 }
